66f5cb7a413e3a3cabdbe43e021afa2121a91408
tony <nguyenhuytoan1994@outlook.com>
2021-09-23 11:13:22 +0700

    Update UI

--- js/script.js
+++ js/script.js
@@ -56,10 +56,7 @@ var swiper = new Swiper(".home-slider", {
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   centeredSlides: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-  },
+  autoplay: false,
   loop: true,
   breakpoints: {
     0: {
